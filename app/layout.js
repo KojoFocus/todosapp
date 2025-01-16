593ddf591dc7e4377cd946225840a64e0b5f3abe
KojoFocus <ernestobimpeh9@gmail.com>
2025-01-16 23:13:07 +0000

    initialized the font

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,12 @@
-import { Geist, Geist_Mono, Luckiest_Guy } from "next/font/google";
+import { Luckiest_Guy } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const LuckiestGuy = Luckiest_Guy({
+const luckiestGuy = Luckiest_Guy({
   variable: "--font-luckiest-guy",
   subsets: ["latin"],
-  weight: 400,
+  weight: '400',
 });
 
 export const metadata = {
@@ -27,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${LuckiestGuy.variable} antialiased`}
+        className={`${luckiestGuy.variable} `}
       >
         <Navbar />
         {children}
