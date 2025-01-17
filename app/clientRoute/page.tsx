f47589d818f91677ab139545a21efa0063b2fc13
KojoFocus <ernestobimpeh9@gmail.com>
2025-01-17 00:12:05 +0000

    created a server component and a client component

---
+++ app/clientRoute/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ClientRoute = () => {
+  console.log("Hi, i have been rendered on the client side");
+  return <div>Client Route</div>;
+};
+
+export default ClientRoute;
---
+++ app/serverRoute/page.jsx
@@ -0,0 +1,8 @@
+"use client";
+
+const ServerPage = () => {
+  console.log("Hi, i have been rendered on the server side");
+  return <div>Server Page</div>;
+};
+
+export default ServerPage;
